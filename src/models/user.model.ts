c4c51bd4e7f763edbc356abe84e6c54a6ee3041c
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-08-14 10:01:37 -0300

    Adding a compare function for the user password

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -41,6 +41,13 @@ userSchema.pre("save", async function (next) {
   return next()
 })
 
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  const user = this as UserDocument
+
+  return bcrypt.compare(candidatePassword, user.password)
+    .catch((e) => false)
+}
+
 const UserModel = mongoose.model<UserDocument>("User", userSchema);
 
 export default UserModel
