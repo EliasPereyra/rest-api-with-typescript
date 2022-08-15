1c75dd43beb3338641afeb1886c22b3171fcaa04
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-08-15 20:35:53 -0300

    Adding a schema for endpoints verifications

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,21 @@
+import { object, string } from 'zod'
+
+export const createUserSchema = object({
+  body: object({
+    name: string({
+      required_error: 'Name is required'
+    }),
+    password: string({
+      required_error: 'Name is required'
+    }).min(6, 'Password too short - should be greater than 6 chars'),
+    passwordConfirmation: string({
+      required_error: 'Name is required'
+    }),
+    email: string({
+      required_error: "Email is required"
+    }).email('Not a valid email'),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ['passwordConfirmation']
+  })
+})
