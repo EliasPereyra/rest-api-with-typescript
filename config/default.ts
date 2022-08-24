537cbfc21e72bc4d50ef43893d0f4258acc15be8
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-08-24 09:44:26 -0300

    Making an access and refresh token

--- config/default.ts
+++ config/default.ts
@@ -5,6 +5,8 @@ export default {
   port: 1337,
   dbUri: process.env.MONGODB_URI,
   saltWorkFactor: 10,
+  accessTokenTtl: '15m',
+  refreshTokenTtl: '1y',
   privateKey: process.env.PRIVATE_KEY,
   publicKey: process.env.PUBLIC_KEY
 }
--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { signJwt } from "../services/jwt.utils";
 import { createSession } from "../services/session.service";
 import { validatePassword } from "../services/user.service";
+import config from 'config'
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // validate the user's password
@@ -11,11 +13,23 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   }
 
   // create a session
-  const session = createSession(user._id, req.get("user-agent") || "")
+  const session = await createSession(user._id, req.get("user-agent") || "")
 
   // create an access token
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get('accessToken') }  // 15 minutes
+  )
 
   // create a refresh token
+  const refreshToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get('refreshToken') }  // 15 minutes
+  )
 
   // return access & refresh tokens
+  return {
+    accessToken,
+    refreshToken
+  }
 }
