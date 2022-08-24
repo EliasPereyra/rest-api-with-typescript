4c9589ebc4ecf734d9eb10789b9ad16b1e8d2d67
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-08-24 08:45:28 -0300

    Adding a function for verifying the token

--- src/services/jwt.utils.ts
+++ src/services/jwt.utils.ts
@@ -11,6 +11,20 @@ export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
   })
 }
 
-function verifyJwt() {
+function verifyJwt(token: string) {
+  try {
+    const decoded = jwt.verify(token, publicKey)
 
+    return {
+      valid: true,
+      expired: false,
+      decoded
+    }
+  } catch (error: any) {
+    return {
+      valid: false,
+      expired: error.message === 'jwt expired',
+      decoded: null
+    }
+  }
 }
