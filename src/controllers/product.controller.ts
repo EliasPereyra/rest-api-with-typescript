71be68130128f7f9329695b0572e66c5d1db0112
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-08-30 09:21:27 -0300

    Adding a product schema

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export async function createProductHandler(req: Request, res: Response) {
+
+}
+
+export async function updateProductHandler(req: Request, res: Response) {
+
+}
+
+export async function getProductHandler(req: Request, res: Response) {
+
+}
+
+export async function deleteProductHandler(req: Request, res: Response) {
+
+}
